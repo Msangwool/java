8494a57bb1065b80d0f359613eb872c0008c189e
sangwoo <tkddnwkdb@gmail.com>
2025-09-07 23:43:02 +0900

    feat: anonymous class, lambda

---
+++ src/mid1/nested/anonymous/ex/Ex0RefMain.java
@@ -0,0 +1,16 @@
+package mid1.nested.anonymous.ex;
+
+public class Ex0RefMain {
+
+    public static void hello(String value) {
+        System.out.println("프로그램 시작");
+        System.out.println("Hello " + value);
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        hello("Java");
+        hello("Spring");
+    }
+
+}
