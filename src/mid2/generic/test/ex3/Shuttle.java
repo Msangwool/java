b9b1c3788d8f14014335571b0c4f7dc957673c19
sangwoo <tkddnwkdb@gmail.com>
2025-09-29 23:41:00 +0900

    feat: generic problems and solutions ex3

---
+++ src/mid2/generic/test/ex3/Shuttle.java
@@ -0,0 +1,20 @@
+package mid2.generic.test.ex3;
+
+import mid2.generic.test.ex3.unit.BioUnit;
+
+public class Shuttle<T extends BioUnit> {
+
+    private T t;
+
+    public void in(T t) {
+        this.t = t;
+    }
+
+    public T out() {
+        return t;
+    }
+
+    public void showInfo() {
+        System.out.println("이름 = " + t.getName() + " HP = " + t.getHp());
+    }
+}
